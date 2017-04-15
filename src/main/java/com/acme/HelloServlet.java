37eea6b16f2b94ed1a64ce55cc4b65a5399273ec
E20 <elopez1@outlook.com>
2017-04-15 15:19:39 -0500

    prueba java sin servlets /test

--- src/main/java/com/acme/HelloServlet.java
+++ src/main/java/com/acme/HelloServlet.java
@@ -3,15 +3,25 @@ package com.acme;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-// com.lei.webapp.quickstart.HelloServlet
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns =
+{ "/test" })
+
+
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
         throws ServletException, IOException {
 
         PrintWriter out = response.getWriter();
-        out.println( "Hello World!" );
+        out.println( "Hello World! sin servlets" );
         out.flush();
         out.close();
     }
